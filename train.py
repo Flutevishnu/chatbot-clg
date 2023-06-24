6af4de4dcbb7071401fd468863d77efa6f48e6c8
vishnu vardhan <88186805+Flutevishnu@users.noreply.github.com>
2023-06-24 12:20:36 +0530

    created a neural network

---
+++ train.py
@@ -0,0 +1,50 @@
+import numpy as np
+import random
+import json
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from nltk_utils import bag_of_words, tokenize, stem
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = [] 
+tags = []
+xy = []
+for  intent in intents["intents"]:
+    tag = intent["tag"]
+    tags.append(tag)
+
+    for pattern in intent["patterns"]:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+       
+ignore_words = ["?", ".", "!"]
+all_words = [stem(w) for w in  all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+# print(len(xy), "patterns")
+# print(len(tags), "tags:", tags)
+# print(len(all_words), "unique stemmed words:", all_words)
+count = 0 
+
+X_train = [] 
+y_train = []
+for(pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+   
+# print(len(X_train))
+# print(len(y_train))
+
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+
