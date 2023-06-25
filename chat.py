1b9a09775a7b87f475872eae582506bb730abefa
vishnu vardhan <88186805+Flutevishnu@users.noreply.github.com>
2023-06-25 14:27:59 +0530

    SU

--- chat.py
+++ chat.py
@@ -26,14 +26,10 @@ model.load_state_dict(model_state)
 model.eval()
 
 bot_name = "KLNCE"
-print("Let's chat! (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
 
-    sentence = tokenize(sentence)
+def get_response(msg):
+     
+    sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
@@ -48,6 +44,19 @@ while True:
     if prob.item() > 0.5:
         for intent in intents['intents']:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                return random.choice(intent["responses"])
     else:
-        print(f"{bot_name}: I do not understand...")
+        return "I don't understand...."
+    
+
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        response = get_response(sentence)
+        print(response)
