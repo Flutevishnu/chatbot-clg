00a564efbc374a3b4468d5c97854798be126227b
vishnu vardhan <flutevishnu@gmail.com>
2023-06-26 11:31:39 +0530

    small updates

--- app.py
+++ app.py
@@ -9,7 +9,5 @@ def home():
     return render_template("index.html")
 
 
-
-
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,15 @@
+function chaticon()
+{
+    console.log("inside the chaticon function");
+    const chatui = document.getElementById("chat-ui");
+
+    if(chatui.style.display != "none")
+    {
+        chatui.style.display != "none";
+    }
+
+    else
+    {
+        chatui.style.display = "block";
+    }
+}
