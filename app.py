33522005c8eb4710df89f970c1c888310e436cab
vishnu vardhan <88186805+Flutevishnu@users.noreply.github.com>
2023-06-26 16:03:26 +0530

    updates

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from chat import get_response
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def home():
     return render_template("index.html")
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer": response}
+    console.log(message)
+    return jsonify(message)
 
 
 if __name__ == "__main__":
--- static/app.js
+++ static/app.js
@@ -2,14 +2,18 @@ function chaticon()
 {
     console.log("inside the chaticon function");
     const chatui = document.getElementById("chat-ui");
+    var input_box = document.querySelector(".input");
+        
 
     if(chatui.style.display != "none")
     {
-        chatui.style.display != "none";
+        chatui.style.display = "none";
+        input_box.style.display = "none";
     }
 
     else
     {
         chatui.style.display = "block";
+        input_box.style.display = "block";
     }
 }
